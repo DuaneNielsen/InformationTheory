15c5d19224872aab317cc4e45751207cb4b095e2
ZEBEAST <ZEBEAST@ZEBEAST-PC>
2016-12-24 20:22:30 -0800

    added entropy and javadoc

--- src/main/java/theory/Ensemble.java
+++ src/main/java/theory/Ensemble.java
@@ -27,15 +27,36 @@ public class Ensemble<T> implements IGenerator {
 		}
 	}
 	
+	/**
+	 * The probability of occurrence of a symbol in the ensemble
+	 * @param symbol
+	 * @return the probability of the occurrence of symbol
+	 */
 	public double probabilityOfOccurence(T symbol) {
 		return probabilityOf.get(symbol);
 	}
 	
-	// returns the Shannon information content in bits, of an isolated occurrence of a member of the alphabet
+	/**
+	 *  returns the Shannon information content in bits, of an isolated occurrence of a member of the alphabet
+	 * @param symbol
+	 * @return Shannon information content for symbol in bits
+	 */
 	public double informationOfOccurence(T symbol) {
 		return log.value(1/probabilityOfOccurence(symbol))/log.value(2);
 	}
 
+	/**
+	 * Shannon entropy is the average information content for the Ensemble
+	 * @return the Shannon entropy of the Ensemble in bits
+	 */
+	public double entropy() {
+		double entropy = 0;
+		for (T symbol : alphabet ) {
+			entropy += informationOfOccurence(symbol);
+		}
+		return entropy/(double)alphabet.length;
+	}
+	
 	// TODO implement hash function using arithmetic coding
 	// the below simply starts from the first on the list and moves along the list in order
 	// this is Order N, but we should be able to do Order 1 or close to it....
